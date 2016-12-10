c4cbbc9418e9da73ba01d159308a968d67e9d9fd
hrwang <henryscut@hotmail.com>
2016-12-10 16:48:58 +0800

    trim function in readsMapping

--- readsMapping.pl
+++ readsMapping.pl
@@ -42,32 +42,46 @@ while (<>){
 	my $reads=$tmp[0];
 	$name=$tmp[1];
 	$name = $name.$suffix;
-
-    system "java -jar $trimmomatic PE -threads $cpu -$phred $reads\_1.fq.gz $reads\_2.fq.gz  $name.1p.fq $name.1u.fq $name.2p.fq $name.2u.fq SLIDINGWINDOW:4:20 LEADING:20 TRAILING:20 MINLEN:30 TOPHRED33 && echo trimmomatic_ok";
-	## check file before and after filtering;
-=head	
-	my @all; my @fq1; my @fq2; my $add;
-	my $out = `du -b $reads\_1.fq.gz`; my $fq1 =  (split /\s/, $out)[0];	
-	$out = `du -b $reads\_1.fq.gz`;    my $fq2 =  (split /\s/, $out)[0];
-	push @all, $fq1+$fq2; push @fq1, $fq1; push @fq2, $fq2;	
-	$out = `du -b $name.1p.fq`; $fq1 = (split /\s/, $out)[0]; push @fq1, $fq1; $add = $fq1;
-    $out = `du -b $name.2p.fq`; $fq2 = (split /\s/, $out)[0]; push @fq2, $fq2; $add+=$fq2;
-	$out = `du -b $name.1u.fq`; $fq1 = (split /\s/, $out)[0]; push @fq1, $fq1; $add+=$fq1;
-    $out = `du -b $name.2u.fq`; $fq2 = (split /\s/, $out)[0]; push @fq2, $fq2; $add+=$fq2;
-	push @all, $add;
-	print "@all\n@fq1\n@fq2\n";
-=cut
-	## Start mapping here.
-    system "$bwa  mem -M -t $cpu -v 1 -R '\@RG\tID:$name\tSM:$name\tLB:$name'  $fa $name.1p.fq $name.2p.fq |$samtools view -bS -  > $name.p.bam && echo bwa_pair_ok && rm $name.1p.fq $name.2p.fq";
-    system "$bwa  mem -M -t $cpu -v 1 -R '\@RG\tID:$name\tSM:$name\tLB:$name'  $fa $name.1u.fq |$samtools view -bS -  > $name.1u.bam && echo bwa_1u_ok && rm $name.1u.fq";
-    system "$bwa  mem -M -t $cpu -v 1 -R '\@RG\tID:$name\tSM:$name\tLB:$name'  $fa $name.2u.fq |$samtools view -bS -  > $name.2u.bam && echo bwa_2u_ok && rm $name.2u.fq";
-    system "$samtools merge -h $name.p.bam $name.bam $name.p.bam $name.1u.bam $name.2u.bam && echo merge_ok && rm $name.p.bam $name.1u.bam $name.2u.bam";
-    system "$samtools fixmate  $name.bam $name\_fixmate.bam  && echo fixmate_ok && rm $name.bam";
-    system "$samtools sort -@ $cpu -O bam -o $name.bam $name\_fixmate.bam && echo fixmate_ok_sort_ok && rm $name\_fixmate.bam";
-	system "$samtools rmdup $name.bam $name.rmdup.bam && echo $name\_rmdup_ok && mv $name.rmdup.bam $name.bam";
-	system "$samtools index $name.bam && echo $name\_index_OK";
-	## I will use GATK to fix gap below
-	system "java -Xmx8g -jar $gatk -T RealignerTargetCreator -R $fa -I $name.bam -o $name.intervals && echo $name\_intervals_ok";
-	system "java -Xmx8g -jar $gatk -T IndelRealigner  -R $fa -I $name.bam -targetIntervals $name.intervals -o $name.gatk.bam && echo $name\_gatk_ok && mv $name.gatk.bam $name.bam";
-	system "$samtools index $name.bam && echo $name\_index_OK";
+	&trimFastq(FASTQ=>[@reads], NAME=>"newName");
+#     system "java -jar $trimmomatic PE -threads $cpu -$phred $reads\_1.fq.gz $reads\_2.fq.gz  $name.1p.fq $name.1u.fq $name.2p.fq $name.2u.fq SLIDINGWINDOW:4:20 LEADING:20 TRAILING:20 MINLEN:30 TOPHRED33 && echo trimmomatic_ok";
+# 	## check file before and after filtering;
+# 
+# 	## Start mapping here.
+#     system "$bwa  mem -M -t $cpu -v 1 -R '\@RG\tID:$name\tSM:$name\tLB:$name'  $fa $name.1p.fq $name.2p.fq |$samtools view -bS -  > $name.p.bam && echo bwa_pair_ok && rm $name.1p.fq $name.2p.fq";
+#     system "$bwa  mem -M -t $cpu -v 1 -R '\@RG\tID:$name\tSM:$name\tLB:$name'  $fa $name.1u.fq |$samtools view -bS -  > $name.1u.bam && echo bwa_1u_ok && rm $name.1u.fq";
+#     system "$bwa  mem -M -t $cpu -v 1 -R '\@RG\tID:$name\tSM:$name\tLB:$name'  $fa $name.2u.fq |$samtools view -bS -  > $name.2u.bam && echo bwa_2u_ok && rm $name.2u.fq";
+#     system "$samtools merge -h $name.p.bam $name.bam $name.p.bam $name.1u.bam $name.2u.bam && echo merge_ok && rm $name.p.bam $name.1u.bam $name.2u.bam";
+#     system "$samtools fixmate  $name.bam $name\_fixmate.bam  && echo fixmate_ok && rm $name.bam";
+#     system "$samtools sort -@ $cpu -O bam -o $name.bam $name\_fixmate.bam && echo fixmate_ok_sort_ok && rm $name\_fixmate.bam";
+# 	system "$samtools rmdup $name.bam $name.rmdup.bam && echo $name\_rmdup_ok && mv $name.rmdup.bam $name.bam";
+# 	system "$samtools index $name.bam && echo $name\_index_OK";
+# 	## I will use GATK to fix gap below
+# 	system "java -Xmx8g -jar $gatk -T RealignerTargetCreator -R $fa -I $name.bam -o $name.intervals && echo $name\_intervals_ok";
+# 	system "java -Xmx8g -jar $gatk -T IndelRealigner  -R $fa -I $name.bam -targetIntervals $name.intervals -o $name.gatk.bam && echo $name\_gatk_ok && mv $name.gatk.bam $name.bam";
+# 	system "$samtools index $name.bam && echo $name\_index_OK";
 }
+
+
+sub trimFastq {
+	my %args = (
+		PAIR    => "PE",
+		THREADS => 1,
+		PHRED   => "phred33",
+		FASTQ   => undef,
+		NAME    => undef,
+		SLIDINGWINDOW => "4:20",
+		LEADING => 20, 
+		TRAILING => 20, 
+		MINLEN  => 30, 
+		OTHER => TOPHRED33,
+		@_,         # actual args override defaults
+	);
+	if (!defined($args{FASTQ}) || !defined ($args{NAME})){
+		die "Fastq array and name are required!\n";
+	}
+	my $trimmomatic="/usr/biobin/trimmomatic-0.36.jar";
+	system "java -jar $trimmomatic $args{PAIR} -threads $args{THREADS} -$args{PHRED} @{$args{FASTQ}}  $args{NAME}.1p.fq $args{NAME}.1u.fq $args{NAME}.2p.fq $args{NAME}.2u.fq SLIDINGWINDOW:$args{SLIDINGWINDOW} LEADING:$args{LEADING} TRAILING:$args{TRAILING} MINLEN:$args{MINLEN} $args{OTHER}";
+	print "$args{NAME} fastq trimming ok";
+}
+
+
